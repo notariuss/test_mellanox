d081f9b412be70766287f35ebee940932a3622fd
Andrii <notariuss.r@gmail.com>
2020-02-14 17:52:25 +0300

    f

--- create_files.py
+++ create_files.py
@@ -8,16 +8,15 @@ Y = 300 # file size
 Data = "./data"
 
 for p in partitions:
-    if X <= (psutil.disk_usage(p.mountpoint).total / 1024 / 1024):
+    if X <= psutil.disk_usage(p.mountpoint).total / 1024 / 1024:
         for i in range(0, Z):
-            with open(f"file{i}", "wb") as f:
-                f.seek(int(300 / 1024) * 1024 * 1024 * 1024)
-                f.write('0')
-                bashCommand = f"dd if={Data} of=file{i}"
+            with open(f"{p.mountpoint}/file{i}", "wb") as f:
+                f.seek(int((300 / 1024) * 1024 * 1024 * 1024))
+                f.write(b'0')
+                bashCommand = f"dd if={Data} of={p.mountpoint}/file{i}"
                 import subprocess
                 import time
-                t1 = time.time() 
+                t1 = time.time()
                 process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
                 output, error = process.communicate()
-                print("file{} seconds spent: {}".format(i, time.time() - t1))
-                
+                print("{}/file{} seconds spent: {}".format(p.mountpoint, i, time.time() - t1))
