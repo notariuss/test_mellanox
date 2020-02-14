3494290a02be3c92ab327bf55d54a43cbb182534
Andrii <notariuss.r@gmail.com>
2020-02-14 17:40:01 +0300

    finished

--- create_files.py
+++ create_files.py
@@ -8,7 +8,7 @@ Y = 300 # file size
 Data = "./data"
 
 for p in partitions:
-    if X <= p.mountpoint, psutil.disk_usage(p.mountpoint).total / 1024 / 1024:
+    if X <= (psutil.disk_usage(p.mountpoint).total / 1024 / 1024):
         for i in range(0, Z):
             with open(f"file{i}", "wb") as f:
                 f.seek(int(300 / 1024) * 1024 * 1024 * 1024)
