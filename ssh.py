3373501eca2e59d8d0700dfde2c8b1a106a752b8
Andrii <notariuss.r@gmail.com>
2020-02-18 15:23:51 +0300

    updated ssh.py

--- ssh.py
+++ ssh.py
@@ -3,6 +3,8 @@ import sys
 from threading import Thread
 import argparse
 import paramiko
+import uuid
+import os
 
 class SSHThread(Thread):
 
@@ -15,40 +17,47 @@ class SSHThread(Thread):
         self.command = command
     
     def run(self):
+        error = False
         client = paramiko.SSHClient()
         client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         try:
             client.connect(hostname=self.host, username=self.username, password=self.secret, port=self.port)
         except paramiko.ssh_exception.AuthenticationException:
             print(f"{self.username}@{self.host} incorect password!")
-            exit()
+            error = True
         except Exception:
             print(f"{self.username}@{self.host} failed to connect!")
-        chan = client.get_transport().open_session()
-        chan.exec_command(self.command)
-        content = open(".ssh_tmp", "wb")
-        errors = open(".ssh_tmp_errors", "wb")
-        while not chan.exit_status_ready():
-            if chan.recv_ready():
-                data = chan.recv(1024)
-                while data:
-                    content.write(data)
+            error = True
+        if not error:
+            chan = client.get_transport().open_session()
+            chan.exec_command(self.command)
+            content_name, errors_name = "." + str(uuid.uuid4()), "." + str(uuid.uuid4())
+            content = open(content_name, "wb")
+            errors = open(errors_name, "wb")
+            while not chan.exit_status_ready():
+                if chan.recv_ready():
                     data = chan.recv(1024)
+                    while data:
+                        content.write(data)
+                        data = chan.recv(1024)
 
-            if chan.recv_stderr_ready():            
-                error_buff = chan.recv_stderr(1024)
-                while error_buff:
-                    errors.write(error_buff)
+                if chan.recv_stderr_ready():            
                     error_buff = chan.recv_stderr(1024)
-        exit_status = chan.recv_exit_status()
-        content.close()
-        errors.close()
-        content = open(".ssh_tmp", "rb")
-        errors = open(".ssh_tmp_errors", "rb")
-        print(f"{self.username}@{self.host} cmd exit status {exit_status}; cmd result:")
-        print(content.read().decode() + errors.read().decode())
-        content.close()
-        errors.close()
+                    while error_buff:
+                        errors.write(error_buff)
+                        error_buff = chan.recv_stderr(1024)
+            exit_status = chan.recv_exit_status()
+            content.close()
+            errors.close()
+            content = open(content_name, "rb")
+            errors = open(errors_name, "rb")
+            print(f"{self.username}@{self.host} cmd exit status {exit_status}; cmd result:")
+            print(content.read().decode() + errors.read().decode())
+            content.close()
+            errors.close()
+            os.remove(content_name)
+            os.remove(errors_name)
+
 
 
 def main(hosts, command):
